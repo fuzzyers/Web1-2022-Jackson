2a76ade92800a351cc82958cbc1aa10569b1b64c
Jackson Williamson <jackiemyson@gmail.com>
2022-09-08 23:02:59 +1200

    javasript hover working state

--- Project/assets/javascript/index.js
+++ Project/assets/javascript/index.js
@@ -1,3 +1,16 @@
-let child = document.querySelector(".buttons")
-let parent = child.parentNode
-console.log("test")
+let btn = document.getElementById("btn1");
+    btn.addEventListener("mouseenter", e => {
+      document.querySelector(".img2").style.opacity = 1;
+    });
+btn.addEventListener("mouseleave", e => {
+      document.querySelector(".img2").style.opacity = 0;
+    });
+
+let btn2 = document.getElementById("btn2");
+    btn2.addEventListener("mouseenter", e => {
+      document.querySelector(".img3").style.opacity = 1;
+    });
+btn2.addEventListener("mouseleave", e => {
+      document.querySelector(".img3").style.opacity = 0;
+    });
+
