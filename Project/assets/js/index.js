9d10900e01c5c8e53eb09c9eb62148064ad236f8
fuzzyers <104363831+fuzzyers@users.noreply.github.com>
2022-09-10 19:05:14 +1200

    added cart counter

--- Project/assets/js/index.js
+++ Project/assets/js/index.js
@@ -1,18 +1,18 @@
 let btn = document.getElementById("btn1");
     btn.addEventListener("mouseenter", e => {
-      document.querySelector("." + "btn1").style.opacity = 1;
+      document.querySelector(".btn1").style.opacity = 1;
     });
 btn.addEventListener("mouseleave", e => {
-      document.querySelector("." + "btn1").style.opacity = 0;
+      document.querySelector(".btn1").style.opacity = 0;
     });
 
 
 let btn2 = document.getElementById("btn2");
     btn2.addEventListener("mouseenter", e => {
-      document.querySelector("." + "btn2").style.opacity = 1;
+      document.querySelector(".btn2").style.opacity = 1;
     });
 btn2.addEventListener("mouseleave", e => {
-      document.querySelector("." + "btn2").style.opacity = 0;
+      document.querySelector(".btn2").style.opacity = 0;
     });
 
 let shopbtn = document.querySelector(".shopbtn");
@@ -20,9 +20,6 @@ shopbtn.addEventListener("mouseenter", e => {
   document.querySelector(".drop").style.display = "block";
 });
 
-
 shopbtn.addEventListener("mouseleave", e => {
-  setTimeout(() => {
     document.querySelector(".drop").style.display = "none";
-  },"1000")
 });
