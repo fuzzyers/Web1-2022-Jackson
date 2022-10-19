907ce4eeb4a118880844bd2de17c2cb6bdac9caa
Jackson Williamson <jackiemyson@gmail.com>
2022-10-19 13:53:44 +1300

    Added and Implemented unsplash api

--- Project/assets/js/index.js
+++ Project/assets/js/index.js
@@ -28,3 +28,22 @@ shopbtn.addEventListener("mouseover", e => {
   })
 });
 
+const BASE_URL = `https://api.unsplash.com`;
+const IMGS = [document.querySelector(".default"),
+document.querySelector(".btn1"),
+document.querySelector(".btn2"),
+document.querySelector(".img4"),
+document.querySelector(".img5"),
+document.querySelector(".img6"),
+document.querySelector(".img7"),
+document.querySelector(".img8")]
+
+
+fetch(`${BASE_URL}/search/photos?query=coffee&client_id=LD9S7v_CkSIIMCO8_sgb1YPCHLrGqAsBJRcLdYQgXsc`)
+ .then(response => response.json())
+ .then(data => {
+  for(let i = 0; i < IMGS.length; i++)
+  {
+    IMGS[i].src = data["results"][i]["urls"]["regular"];
+  }
+}) 
