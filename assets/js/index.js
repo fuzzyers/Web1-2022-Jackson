b34aafaa404ecd7f491166ec74626de2966d7f8f
fuzzyers <jackiemyson@gmail.com>
2022-11-14 13:27:54 +1300

    roller door working not broken

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,7 +28,6 @@ let shopbtn = document.querySelector(".shopbtn");
   })
 });
 
-
 const BASE_URL = `https://api.unsplash.com`;
 const IMGS = [document.querySelector(".default"),
 document.querySelector(".btn1"),
@@ -48,3 +47,42 @@ fetch(`${BASE_URL}/search/photos?query=coffee&client_id=LD9S7v_CkSIIMCO8_sgb1YPC
     IMGS[i].src = data["results"][i]["urls"]["regular"];
   }
 }) 
+
+let rollerleft = document.querySelector(".banner3");
+let rollerright = document.querySelector(".banner4")
+let content1 = document.querySelector(".contents1");
+let content2 = document.querySelector(".contents2");
+let base2 = document.querySelector(".base2");
+let img1 = document.querySelector(".img6");
+
+  rollerleft.addEventListener("mouseover", e => {
+    content1.style.left = "90px"
+  })
+  rollerleft.addEventListener("mouseleave", e =>{
+    content1.style.left = "100px"
+  })
+
+  rollerright.addEventListener("click", e =>{
+    base2.style.right = "calc(100% - 244px)"
+    rollerright.style.borderRight = "1px solid black"
+    rollerright.style.borderLeft = "none"
+    content1.style.visibility = "hidden"
+    content2.style.visibility = "visible"
+    content2.style.width = "auto"
+    content2.style.height = "auto"
+    content1.style.width = "0"
+    content1.style.height = "0"
+  })
+
+  rollerleft.addEventListener("click", e =>{
+    base2.style.right = "0"
+    rollerright.style.borderRight = "none"
+    rollerright.style.borderLeft = "1px solid black"
+    content1.style.visibility = "visible"
+    content1.style.width = "auto"
+    content1.style.height = "auto"
+    content2.style.visibility = "hidden"
+    content2.style.width = "0"
+    content2.style.height = "0"
+  })
+
