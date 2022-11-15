7df1b8f7ad1935444fadff77ae0f640b369257f3
Jackson Williamson <jackiemyson@gmail.com>
2022-11-16 00:26:10 +1300

    added js scroll blur animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,5 @@
-let btn = document.getElementById("btn1");
-    btn.addEventListener("mouseenter", e => {
-      document.querySelector(".btn1").style.opacity = 1;
-    });
-btn.addEventListener("mouseleave", e => {
-      document.querySelector(".btn1").style.opacity = 0;
-    });
-
-
-let btn2 = document.getElementById("btn2");
-    btn2.addEventListener("mouseenter", e => {
-      document.querySelector(".btn2").style.opacity = 1;
-    });
-btn2.addEventListener("mouseleave", e => {
-      document.querySelector(".btn2").style.opacity = 0;
-    });
-
-let shopbtn = document.querySelector(".shopbtn");
-  shopbtn.addEventListener("mouseover", e => {
-  document.querySelector(".drop").style.display = "block";
-  shopbtn = document.querySelector(".shopdrop")
-  shopbtn.addEventListener("mouseenter", e => {
-    document.querySelector(".shopdrop").style.display = "block";
-  })
-  shopbtn.addEventListener("mouseleave", e => {
-    document.querySelector(".drop").style.display = "none";
-
-  })
-});
-
 const BASE_URL = `https://api.unsplash.com`;
+const blurani = document.querySelector(".center");
 const IMGS = [document.querySelector(".default"),
 document.querySelector(".btn1"),
 document.querySelector(".btn2"),
@@ -37,7 +8,40 @@ document.querySelector(".img5"),
 document.querySelector(".img6"),
 document.querySelector(".img7"),
 document.querySelector(".img8")]
+let btn = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let shopbtn = document.querySelector(".shopbtn");
+let rollerleft = document.querySelector(".banner3");
+let rollerright = document.querySelector(".banner4")
+let content1 = document.querySelector(".contents1");
+let content2 = document.querySelector(".contents2");
+let base2 = document.querySelector(".base2");
+let img1 = document.querySelector(".img6");
 
+btn.addEventListener("mouseenter", e => {
+      document.querySelector(".btn1").style.opacity = 1;
+    });
+btn.addEventListener("mouseleave", e => {
+      document.querySelector(".btn1").style.opacity = 0;
+    });
+
+    btn2.addEventListener("mouseenter", e => {
+      document.querySelector(".btn2").style.opacity = 1;
+    });
+btn2.addEventListener("mouseleave", e => {
+      document.querySelector(".btn2").style.opacity = 0;
+    });
+
+  shopbtn.addEventListener("mouseover", e => {
+  document.querySelector(".drop").style.display = "block";
+  shopbtn = document.querySelector(".shopdrop")
+  shopbtn.addEventListener("mouseenter", e => {
+    document.querySelector(".shopdrop").style.display = "block";
+  })
+  shopbtn.addEventListener("mouseleave", e => {
+    document.querySelector(".drop").style.display = "none";
+  })
+});
 
 fetch(`${BASE_URL}/search/photos?query=coffee&client_id=LD9S7v_CkSIIMCO8_sgb1YPCHLrGqAsBJRcLdYQgXsc`)
  .then(response => response.json())
@@ -47,13 +51,6 @@ fetch(`${BASE_URL}/search/photos?query=coffee&client_id=LD9S7v_CkSIIMCO8_sgb1YPC
     IMGS[i].src = data["results"][i]["urls"]["regular"];
   }
 }) 
-
-let rollerleft = document.querySelector(".banner3");
-let rollerright = document.querySelector(".banner4")
-let content1 = document.querySelector(".contents1");
-let content2 = document.querySelector(".contents2");
-let base2 = document.querySelector(".base2");
-let img1 = document.querySelector(".img6");
 
   rollerleft.addEventListener("mouseover", e => {
     content1.style.left = "90px"
@@ -86,3 +83,24 @@ let img1 = document.querySelector(".img6");
     content2.style.height = "0"
   })
 
+  function isInViewport(item) {
+
+    let bounding = item.getBoundingClientRect(),
+        myElementHeight = item.offsetHeight,
+        myElementWidth = item.offsetWidth;
+
+    if(bounding.top >= -myElementHeight
+        && bounding.left >= -myElementWidth
+        && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) + myElementWidth
+        && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + myElementHeight) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+window.addEventListener("scroll", function(){
+   if(isInViewport(blurani)) {
+      blurani.style.animation = "blur 5s"; 
+   }
+});
