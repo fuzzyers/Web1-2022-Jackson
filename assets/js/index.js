f200f3e65e867c046bc9d79e4d5869dc2a57d1e0
fuzzyers <104363831+fuzzyers@users.noreply.github.com>
2022-11-18 12:52:11 +1300

    SearchBar Implemented

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,55 +23,55 @@ let arrow = document.querySelectorAll(".arrow");
 
 //  Event for handling navbar dropdown menu
 btn.addEventListener("mouseenter", e => {
-      document.querySelector(".btn1").style.opacity = 1;
-    });
+  document.querySelector(".btn1").style.opacity = 1;
+});
 btn.addEventListener("mouseleave", e => {
-      document.querySelector(".btn1").style.opacity = 0;
-    });
-
-    btn2.addEventListener("mouseenter", e => {
-      document.querySelector(".btn2").style.opacity = 1;
-    });
-btn2.addEventListener("mouseleave", e => {
-      document.querySelector(".btn2").style.opacity = 0;
-    });
-
-  shopbtn.addEventListener("mouseover", e => {
-  document.querySelector(".drop").style.display = "block";
-  shopbtn = document.querySelector(".shopdrop")
-  shopbtn.addEventListener("mouseenter", e => {
-    document.querySelector(".shopdrop").style.display = "block";
-  })
-  shopbtn.addEventListener("mouseleave", e => {
-    document.querySelector(".drop").style.display = "none";
-  })
+  document.querySelector(".btn1").style.opacity = 0;
 });
 
-// Event for Showing "Read" text above image on hover
-imghover.forEach((image) => {
-    image.addEventListener("mouseover", e => {
-      image.nextElementSibling.style.visibility = "visible"
+btn2.addEventListener("mouseenter", e => {
+  document.querySelector(".btn2").style.opacity = 1;
+});
+btn2.addEventListener("mouseleave", e => {
+  document.querySelector(".btn2").style.opacity = 0;
     });
-    image.addEventListener("mouseleave", e => {
-      image.nextElementSibling.style.visibility = "hidden"
+    
+    shopbtn.addEventListener("mouseover", e => {
+      document.querySelector(".drop").style.display = "block";
+      shopbtn = document.querySelector(".shopdrop")
+      shopbtn.addEventListener("mouseenter", e => {
+        document.querySelector(".shopdrop").style.display = "block";
+      })
+      shopbtn.addEventListener("mouseleave", e => {
+        document.querySelector(".drop").style.display = "none";
+      })
+    });
+    
+    // Event for Showing "Read" text above image on hover
+    imghover.forEach((image) => {
+      image.addEventListener("mouseover", e => {
+        image.nextElementSibling.style.visibility = "visible"
+      });
+      image.addEventListener("mouseleave", e => {
+        image.nextElementSibling.style.visibility = "hidden"
+      })
     })
+    
+    
+    // Event for Showing arrows on hover
+    workgroup.forEach((image) => {
+      image.addEventListener("mouseover", e => {
+        image.firstChild.nextElementSibling.nextElementSibling.nextElementSibling.style.visibility = "visible"
+      });
+      image.addEventListener("mouseleave", e => {
+        image.firstChild.nextElementSibling.nextElementSibling.nextElementSibling.style.visibility = "hidden"
+      })
 })
 
-
-// Event for Showing arrows on hover
-workgroup.forEach((image) => {
-  image.addEventListener("mouseover", e => {
-    image.firstChild.nextElementSibling.nextElementSibling.nextElementSibling.style.visibility = "visible"
-  });
-  image.addEventListener("mouseleave", e => {
-    image.firstChild.nextElementSibling.nextElementSibling.nextElementSibling.style.visibility = "hidden"
-  })
-})
-  
 // Image Api it assigns random images to an array of images
 fetch(`${BASE_URL}/search/photos?query=coffee&client_id=LD9S7v_CkSIIMCO8_sgb1YPCHLrGqAsBJRcLdYQgXsc`)
- .then(response => response.json())
- .then(data => {
+.then(response => response.json())
+.then(data => {
   for(let i = 0; i < IMGS.length; i++)
   {
     IMGS[i].src = data["results"][i]["urls"]["regular"];
@@ -79,48 +79,63 @@ fetch(`${BASE_URL}/search/photos?query=coffee&client_id=LD9S7v_CkSIIMCO8_sgb1YPC
 }) 
 
 // Handles oppening and closing roller doors
-  rollerright.addEventListener("click", e =>{
-    base2.style.right = "calc(100% - 244px)"
-    rollerright.style.borderRight = "1px solid #006eb6"
-    rollerright.style.borderLeft = "none"
-    content2.style.visibility = "visible"
-    content2.style.width = "auto"
-    content2.style.height = "auto"
-    content1.style.visibility = "hidden"
-    content1.style.width = "0"
-    content1.style.height = "0"
-  })
+rollerright.addEventListener("click", e =>{
+  base2.style.right = "calc(100% - 244px)"
+  rollerright.style.borderRight = "1px solid #006eb6"
+  rollerright.style.borderLeft = "none"
+  content2.style.visibility = "visible"
+  content2.style.width = "auto"
+  content2.style.height = "auto"
+  content1.style.visibility = "hidden"
+  content1.style.width = "0"
+  content1.style.height = "0"
+})
 
-  rollerleft.addEventListener("click", e =>{
-    base2.style.right = "0"
-    rollerright.style.borderRight = "none"
-    rollerright.style.borderLeft = "1px solid #006eb6"
-    content1.style.visibility = "visible"
-    content1.style.width = "auto"
-    content1.style.height = "auto"
-    content2.style.visibility = "hidden"
-    content2.style.width = "0"
-    content2.style.height = "0"
-  })
+rollerleft.addEventListener("click", e =>{
+  base2.style.right = "0"
+  rollerright.style.borderRight = "none"
+  rollerright.style.borderLeft = "1px solid #006eb6"
+  content1.style.visibility = "visible"
+  content1.style.width = "auto"
+  content1.style.height = "auto"
+  content2.style.visibility = "hidden"
+  content2.style.width = "0"
+  content2.style.height = "0"
+})
 
-  // Checks viewport and assigns blur animation effect on image.
-  function isInViewport(item) {
-    let bounding = item.getBoundingClientRect(),
-        myElementHeight = item.offsetHeight,
-        myElementWidth = item.offsetWidth;
-
-    if(bounding.top >= -myElementHeight
-        && bounding.left >= -myElementWidth
-        && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) + myElementWidth
-        && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + myElementHeight) {
-        return true;
+// Checks viewport and assigns blur animation effect on image.
+function isInViewport(item) {
+  let bounding = item.getBoundingClientRect(),
+  myElementHeight = item.offsetHeight,
+  myElementWidth = item.offsetWidth;
+  
+  if(bounding.top >= -myElementHeight
+    && bounding.left >= -myElementWidth
+    && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) + myElementWidth
+    && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + myElementHeight) {
+      return true;
     } else {
-        return false;
+      return false;
     }
-}
-
-window.addEventListener("scroll", function(){
-   if(isInViewport(blurani)) {
+  }
+  
+  window.addEventListener("scroll", function(){
+    if(isInViewport(blurani)) {
       blurani.style.animation = "blur 5s"; 
-   }
-});
+    }
+  });
+
+  let searchbutton = document.querySelector(".searchbtn")
+  let searchbar = document.querySelector(".Searchbar")
+  let searchbaractive = false;
+
+  searchbutton.addEventListener("click", e =>{
+    if (!searchbaractive){
+      searchbar.style.display = "block";
+      searchbaractive = true
+    }
+    else {
+      searchbar.style.display = "none"
+      searchbaractive = false
+    }
+  })
